dea9effdc3ef4b0cb6d3f350812269be157b726a
mathieuHa <mathieu.hanotaux@gmail.com>
2017-03-11 15:14:13 +0100

    Remote Controle Servo From Web

--- controle_arduino.ino.ino
+++ controle_arduino.ino.ino
@@ -10,31 +10,29 @@
 #define MILIEU  90
 #define MIN_X   30
 #define MAX_X   150
-#define MIN_Y   30
+#define MIN_Y   40
 #define MAX_Y   150
-#define SPEED   10
+#define SPEED   20
+#define SLEEP   300
 
 char message;
 
 Servo servo_x;
 Servo servo_y;
 
+int pos_x;
+int pos_y;
+
 void setup() {
   
   message = ' ';  
   pinMode(LED_BUILTIN, OUTPUT);
-  
-  servo_x.attach(SERVO_X);
-  servo_y.attach(SERVO_Y);
-  servo_x.write(MILIEU);
-  servo_y.write(MILIEU);
-  delay(300);
-  servo_x.detach();
-  servo_y.detach();
-  
+  pos_x = MILIEU;
+  pos_y = MILIEU;
+  init_servo();
   Serial.begin(9600);
   Serial.print("Lancement du programme !");
-  delay(3000);
+  delay(1000);
 }
 
 void loop() {
@@ -52,7 +50,7 @@ void loop() {
       case DROITE:
         //Tourner à droite
         Serial.println("DROITE");
-        turn(SPEED, DROITE, servo_x);
+        turn(-SPEED, DROITE, servo_x);
       break;
       case HAUT:
         //Tourner vers le haut
@@ -62,7 +60,7 @@ void loop() {
       case BAS:
         //Tourner vers le BAS
         Serial.println("BAS");
-        turn(SPEED, BAS, servo_y);
+        turn(-SPEED, BAS, servo_y);
       break;
     }
   }
@@ -74,38 +72,83 @@ void turn(int value,char Direction, Servo servo)
   switch(Direction)
   {
     case GAUCHE:
-      if (servo.read() + value >=  MIN_X){
+      if (pos_x + value <=  MAX_X){
+        Serial.println("GO GAUCHE");
+        pos_x+=value;
+        Serial.println(pos_x);
         servo.attach(SERVO_X);
-        servo.write(servo.read()+value);
+        servo.write(pos_x);
         delay(200);
         servo.detach();
+      } else {
+        Serial.println("Limite GAUCHE atteinte.");
       }
         break;
     case DROITE:    
-      if (servo.read() + value <=  MAX_X){
+      if (pos_x + value >=  MIN_X){
+        Serial.println("GO DROITE");
+        pos_x+=value;
+        Serial.println(pos_x);
         servo.attach(SERVO_X);
-        servo.write(servo.read()+value);
+        servo.write(pos_x);
         delay(200);
         servo.detach();
+      } else {
+        Serial.println("Limite DROITE atteinte.");
       }
         break;
     case HAUT:
-      if (servo.read() + value <=  MAX_Y){
+      if (pos_y + value <=  MAX_Y){
+        Serial.println("GO HAUT");
+        pos_y+=value;
+        Serial.println(pos_y);
         servo.attach(SERVO_Y);
-        servo.write(servo.read()+value);
+        servo.write(pos_y);
         delay(200);
         servo.detach();
+      } else {
+        Serial.println("Limite HAUT atteinte.");
       }
         break;
     case BAS:    
-      if (servo.read() + value >=  MIN_Y){
+      if (pos_y + value >=  MIN_Y){
+        Serial.println("GO BAS");
+        pos_y+=value;
+        Serial.println(pos_y);
         servo.attach(SERVO_Y);
-        servo.write(servo.read()+value);
+        servo.write(pos_y);
         delay(200);
         servo.detach();    
+      } else {
+        Serial.println("Limite BAS atteinte.");
       }
         break;
   }  
 }
+
+void init_servo ()
+{
+  servo_x.attach(SERVO_X);
+  servo_y.attach(SERVO_Y);
+  servo_x.write(MILIEU);
+  servo_y.write(MILIEU);
+  delay(SLEEP);
+  servo_x.write(MILIEU+30);
+  delay(SLEEP);
+  servo_x.write(MILIEU-30);
+  delay(SLEEP);
+  servo_x.write(MILIEU);
+  delay(SLEEP);
+  servo_y.write(MILIEU+30);
+  delay(SLEEP);
+  servo_y.write(MILIEU-30);
+  delay(SLEEP);
+  servo_y.write(MILIEU);
+  delay(SLEEP);
+  servo_x.detach();
+  servo_y.detach();
+}
+
+
 
 
