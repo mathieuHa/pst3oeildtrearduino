bead959503e5b2b83deb8efe8ddf060067e8b438
mathieuHa <mathieu.hanotaux@gmail.com>
2017-05-12 19:35:55 +0200

    addd pic.sh

--- pic.sh
+++ pic.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-sudo echo 'im 1' > /var/www/html/camera/FIFO
+echo 'im 1' > /var/www/html/camera/FIFO
 
+sleep 0.2
+
+path="/var/www/html/media/"
+cd /var/www/html/media/
+
+th=((ls *.jpg -rt | head -1))
+img=((ls *.jpg -rt | head -2 | tail -n 1))
+
+mv ${th} path/${1}/
+mv ${img} path/${1}/
