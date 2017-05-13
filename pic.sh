046de6687ddcb35be0a3585bf80cadd1227349bc
mathieuHa <mathieu.hanotaux@gmail.com>
2017-05-13 19:14:37 +0200

    update pic.sh

--- pic.sh
+++ pic.sh
@@ -4,11 +4,11 @@ echo 'im 1' > /var/www/html/camera/FIFO
 
 sleep 0.2
 
-path="/var/www/html/media/"
-cd /var/www/html/media/
+path="/var/www/html/camera/media/"
+cd ${path}
 
-th=((ls *.jpg -rt | head -1))
-img=((ls *.jpg -rt | head -2 | tail -n 1))
+th=`ls *.jpg -rt | head -1`
+img=`ls *.jpg -rt | head -2 | tail -n 1`
 
-mv ${th} path/${1}/
-mv ${img} path/${1}/
+mv ${th} path/${1}/${th}
+mv ${img} path/${1}/${img}
